fbacc183031e760e5229360aeb6d5c0149242534
yotu <114195789+ulxsth@users.noreply.github.com>
2024-01-06 18:00:44 +0900

    update: ゲーム中は速度上昇とジャンプ力上昇がつくようにした

--- src/main/java/com/ulxsth/hanamiroyale/models/Game.java
+++ src/main/java/com/ulxsth/hanamiroyale/models/Game.java
@@ -2,6 +2,8 @@ package com.ulxsth.hanamiroyale.models;
 
 import com.ulxsth.hanamiroyale.HanamiRoyaleMain;
 import org.bukkit.Bukkit;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
 public class Game {
     private static final int TIME = 20;
@@ -11,16 +13,22 @@ public class Game {
 
     public void startTimer() {
         this.time = TIME;
-        Bukkit.getScheduler().scheduleSyncRepeatingTask(plugin, new Runnable() {
-            @Override
-            public void run() {
-                if(time > 0) {
-                    plugin.getLogger().info("Time left: " + time);
-                    time--;
-                } else {
-                    plugin.getLogger().info("Time's up!");
-                    Bukkit.getScheduler().cancelTasks(plugin);
-                }
+        Bukkit.getScheduler().scheduleSyncRepeatingTask(plugin, () -> {
+            plugin.getLogger().info("Time left: " + time);
+            if(time > 0) {
+                Bukkit.getOnlinePlayers().forEach(player -> {
+                    player.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 20, 1));
+                    player.addPotionEffect(new PotionEffect(PotionEffectType.JUMP, 20, 1));
+                });
+                time--;
+            } else {
+                plugin.getLogger().info("Time's up!");
+                Bukkit.getOnlinePlayers().forEach(player -> {
+                    player.getActivePotionEffects().forEach(potionEffect -> {
+                        player.removePotionEffect(potionEffect.getType());
+                    });
+                });
+                Bukkit.getScheduler().cancelTasks(plugin);
             }
         }, 0L, 20L);
     }
